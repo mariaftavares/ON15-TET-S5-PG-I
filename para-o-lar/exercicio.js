dce71ccec853416e752535574d1527f0ee2772cc
Maria Fernanda Tavares <mariaffernandatavares@gmail.com>
2022-04-30 11:12:12 -0300

    Subindo exercicio

--- para-o-lar/exercicio.js
+++ para-o-lar/exercicio.js
@@ -1,23 +1,20 @@
 class dnaTransformacao {
-    paraRna(dna){
-     this.dna = dna.split("");
-     this.rna = [];
-        for (let sequencia of this.dna){
-                if (sequencia.toLowerCase() == "g"){
-                    this.rna.push("C")
-                }
-                else if (sequencia.toLowerCase() == "c"){
-                        this.rna.push("G")
-                }
-                else if (sequencia.toLowerCase() == "t"){
-                    this.rna.push("A")
-                }
-                else if (sequencia.toLowerCase() == "a"){
-                    this.rna.push("U")
-                }
-            
+    paraRna(dna) {
+        this.dna = dna.split("");
+        this.rna = [];
+        for (let sequencia of this.dna) {
+            if (sequencia.toLowerCase() == "g") {
+                this.rna.push("C")
+            } else if (sequencia.toLowerCase() == "c") {
+                this.rna.push("G")
+            } else if (sequencia.toLowerCase() == "t") {
+                this.rna.push("A")
+            } else if (sequencia.toLowerCase() == "a") {
+                this.rna.push("U")
+            }
+
         }
-            return this.rna.join("");
+        return this.rna.join("");
     }
 }
 
@@ -27,6 +24,3 @@ console.log(teste.paraRna("G"));
 console.log(teste.paraRna("A"));
 console.log(teste.paraRna("T"));
 console.log(teste.paraRna("ACGTGGTCTTAA"));
-
-
-
